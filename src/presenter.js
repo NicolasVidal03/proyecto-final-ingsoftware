cb17acdaf84d97082dcf76e20c23f66579dbcfc5
Oswi142 <edwin.roman@ucb.edu.bo>
2023-10-26 09:34:59 -0400

    Se puede editar el estado de una kata y se puede lo puede

--- src/presenter.js
+++ src/presenter.js
@@ -120,6 +120,7 @@ function editarKata(pos) {
   document.querySelector("#editar-nombre-autor").value = lista.getLista()[pos].getAutor();
   document.querySelector("#editar-desc-kata").value = lista.getLista()[pos].getDescripcion();
   document.querySelector("#editar-dificultad-kata").value = lista.getLista()[pos].getDificultad();
+  document.querySelector("#editar-estado-kata").value = lista.getLista()[pos].getEstado();
 
   form_editarKata.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -127,6 +128,7 @@ function editarKata(pos) {
     lista.getLista()[pos].setAutor(document.querySelector("#editar-nombre-autor").value);
     lista.getLista()[pos].setDescripcion(document.querySelector("#editar-desc-kata").value);
     lista.getLista()[pos].setDificultad(document.querySelector("#editar-dificultad-kata").value);
+    lista.getLista()[pos].setEstado(document.querySelector("#editar-estado-kata").value);
     form_editarKata.classList.add('hide');
     catalogoCompleto.innerHTML = "";
     lista.getLista().forEach(mostrarCatalogoKatas); 
